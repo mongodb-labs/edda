63a43d59335d1e8a0912d45cd1f7ee62f7dd6606
Samantha Ritter <samantha.ritter@10gen.com>
2012-06-08 14:54:27 -0400

    added a template for new modules

---
+++ logl/modules/template.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env
+#------------------------------------------------------------
+# This module processes WHICH types of log lines.
+#------------------------------------------------------------
+
+
+
+#------------------------------------------------------------
+
+# does the given log line fit the criteria for this module?
+# return True if yes, False if no.
+def criteria():
+
+#------------------------------------------------------------
+
+# if the given log line fits the criteria for this module,
+# processes the line and creates a document for it.
+    # DESCRIBE DOCUMENT FORMAT HERE
+def process():
+
+#------------------------------------------------------------
+
+
