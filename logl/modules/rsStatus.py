344dc75904c09c6d42187347fcfbced9b87cedb4
Samantha Ritter <samantha.ritter@10gen.com>
2012-06-14 10:57:32 -0400

    added methods to identify STARTUP1 and STARTUP2 states

--- logl/modules/rsStatus.py
+++ logl/modules/rsStatus.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""This module processes WHICH types of log lines."""
+"""This module processes RS STATUS CHANGE types of log lines."""
 
 import string
 import re
@@ -13,14 +13,25 @@ def criteria(msg):
         return 1
 
     # check for rsHealthPoll msg
-    if (string.find(msg, '[rsHealthPoll]') >= 0):
-        if (string.find(msg, 'replSet') >= 0):
-            return 2
+    if (string.find(msg, '[rsHealthPoll] replSet') >= 0):
+        return 2
 
 
 def process(msg, date):
     """if the given log line fits the critera for this module,
-    processes the line and creates a document for it."""
+    processes the line and creates a document for it.
+    document = {
+       "date" : date,
+       "type" : "status",
+       "msg" : msg,
+       "origin_server" : name,
+       "info" : {
+          "subtype" : None,
+          "status" : status,
+          "status_code" : int,
+          "server" : "host:port",
+          }
+    }"""
 
     result = criteria(msg)
     if result < 0:
@@ -44,10 +55,25 @@ def process(msg, date):
 
 def rs_start(msg, doc):
     """this replica set is starting up.  Capture host information."""
+
+    # capture host information from state STARTUP1
+    place = string.find(msg, "I am")
+    if (place >= 0):
+        doc["info"]["server"] = msg[place + 6:]
+        doc["info"]["status"] = "STARTUP1"
+        doc["info"]["status_code"] = 0
+        return doc
+    # capture information for state STARTUP2
+    place = string.find(msg, "STARTUP2")
+    if (place >= 0):
+        doc["info"]["server"] = "self"
+        doc["info"]["status"] = "STARTUP2"
+        doc["info"]["status_code"] = 5
+        return doc
     return None
 
 
 def rs_health(msg, doc):
     """this replica set is reporting a health change."""
-    return None
+    return doc
 
