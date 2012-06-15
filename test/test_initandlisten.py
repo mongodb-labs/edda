b195bcb027f99a010bcb7aed5d0cc22a71de3464
Samantha Ritter <samantha.ritter@10gen.com>
2012-06-15 13:30:02 -0400

    changed asserts to check against exact return value

--- test/test_initandlisten.py
+++ test/test_initandlisten.py
@@ -5,16 +5,16 @@ from datetime import datetime
 def test_criteria():
     """Test the criteria() method of this module"""
     # these should not pass
-    assert criteria("this should not pass") < 0
-    assert criteria("Mon Jun 11 15:56:40 [conn5] end connection 127.0.0.1:55224 (2 connections now open)") < 0
-    assert criteria("Mon Jun 11 15:56:16 [initandlisten] ** WARNING: soft rlimits too low. Number of files is 256, should be at least 1000") < 0
-    assert criteria("init and listen starting") < 0
-    assert criteria("[initandlisten]") < 0
-    assert criteria("starting") < 0
-    assert criteria("connection accepted") < 0
+    assert criteria("this should not pass") == -1
+    assert criteria("Mon Jun 11 15:56:40 [conn5] end connection 127.0.0.1:55224 (2 connections now open)") == -1
+    assert criteria("Mon Jun 11 15:56:16 [initandlisten] ** WARNING: soft rlimits too low. Number of files is 256, should be at least 1000") == -1
+    assert criteria("init and listen starting") == -1
+    assert criteria("[initandlisten]") == -1
+    assert criteria("starting") == -1
+    assert criteria("connection accepted") == -1
     # these should pass
-    assert criteria("Mon Jun 11 15:56:16 [initandlisten] MongoDB starting : pid=7029 port=27018 dbpath=/data/rs2 64-bit host=Kaushals-MacBook-Air.local") > 0
-    assert criteria("Mon Jun 11 15:56:24 [initandlisten] connection accepted from 127.0.0.1:55227 #6 (4 connections now open)") > 0
+    assert criteria("Mon Jun 11 15:56:16 [initandlisten] MongoDB starting : pid=7029 port=27018 dbpath=/data/rs2 64-bit host=Kaushals-MacBook-Air.local") == 1
+    assert criteria("Mon Jun 11 15:56:24 [initandlisten] connection accepted from 127.0.0.1:55227 #6 (4 connections now open)") == 2
     return
 
 
