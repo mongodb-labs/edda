52a6fcd1b0133b32e9c958681b325a1bf98a5744
Kaushal Parikh <kaushalparikh@Kaushals-MacBook-Air.local>
2012-06-15 10:44:24 -0400

    added tests

--- test/test_rsSync.py
+++ test/test_rsSync.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from logl.modules.rsSync import *
 
 def test_criteria():
@@ -6,7 +6,18 @@ def test_criteria():
     assert criteria("Tue Jun 12 13:08:47 [rsSync] replSet to: localhost:27017") == -1
 
 def test_process():
-	pass
+
+    doc =  process("Tue Jun 12 13:08:47 [rsSync] replSet syncing to: localhost:27017", datetime.now())
+    assert doc
+    assert doc["type"] == 'sync'
+    assert process("This should fail", datetime.now()) == None
+
 
 def test_syncingDiff():
-    pass
+
+    currTime = datetime.now()
+    test = syncingDiff("Tue Jun 12 13:08:47 [rsSync] replSet syncing to: localhost:27017", 
+    	process("Tue Jun 12 13:08:47 [rsSync] replSet syncing to: localhost:27017", currTime))
+    assert test
+    assert test["type"] == 'sync'
+    assert syncingDiff("This should fail", datetime.now()) == test
