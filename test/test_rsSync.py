51778ed8d85aa3d1bb0936e01aa37b72eea0b8ff
Kaushal Parikh <kaushalparikh@Kaushals-MacBook-Air.local>
2012-06-15 13:28:32 -0400

    added tests

--- test/test_rsSync.py
+++ test/test_rsSync.py
@@ -1,13 +1,23 @@
 from datetime import datetime
 from logl.modules.rsSync import *
 
+
 def test_criteria():
     assert criteria("Tue Jun 12 13:08:47 [rsSync] replSet syncing to: localhost:27017") == 1
+    #should fail, absence of word "syncing": malformed message
     assert criteria("Tue Jun 12 13:08:47 [rsSync] replSet to: localhost:27017") == -1
+    #should fail, absence of [rsSync]: malformed message
+    assert criteria("Tue Jun 12 13:08:47 replSet to: localhost:27017") == -1
+    #should pass, it doesn't test to see if there is a valid port number until test_syncingDiff: malformed message to fail at another point
+    assert criteria("Tue Jun 12 13:08:47 [rsSync] replSet syncing to:") == 1
+    #should pass in this situation, date is irrevealant
+    assert criteria("[rsSync] replSet syncing to: localhost:27017") == 1
+    #foo bar test from git
+    assert criteria("foo bar") == -1
 
 def test_process():
 
-    doc =  process("Tue Jun 12 13:08:47 [rsSync] replSet syncing to: localhost:27017", datetime.now())
+    doc = process("Tue Jun 12 13:08:47 [rsSync] replSet syncing to: localhost:27017", datetime.now())
     assert doc
     assert doc["type"] == 'sync'
     assert process("This should fail", datetime.now()) == None
