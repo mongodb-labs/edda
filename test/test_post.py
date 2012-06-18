c4ee0ead717e8d649a03e7bc6bdad1ffe3309991
Samantha Ritter <samantha.ritter@10gen.com>
2012-06-18 17:09:06 -0400

    Add a testing file for post.py

---
+++ test/test_post.py
@@ -0,0 +1,18 @@
+from logl.post import *
+import pymongo
+from datetime import timedelta
+
+def test_server_clock_skew():
+    pass
+
+
+def test_detect():
+    pass
+
+
+def test_clock_skew_doc():
+    pass
+
+
+def test_server_matchup():
+    pass
