7f0d804a53bde7d82236115463ba7f576bc1f53b
Kaushal Parikh <kaushal.parikh@10gen.com>
2012-06-19 12:16:54 -0400

    Basic server matchup. Doesn't work yet.

--- logl/post/server_matchup.py
+++ logl/post/server_matchup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import pymongo
 
 
 def server_matchup(db, collName):
@@ -18,4 +19,25 @@ def server_matchup(db, collName):
     # ---------------> find all status messages from other servers about a with same state
     # ---------------> check if the other servers' messages align in time (with clock skew) and are about the same unnamed server
     # ---------------> if other times agree, take that as this server's name, and calculate clock skew with it.
+
+    for doc in collName[servers].find()
+        if doc["server_name"] == "unknown"
+            cursor = collName[.entries].find({"origin_server" : doc["server_num"], "type": "status"})
+            if cursor.alive():
+                while True:
+                    code = doc["info"]["state_code"]
+                    if code == 2 or code == 5:
+                        if cursor.alive():
+                            cursor = cursor.next()
+                            continue
+                    else
+                        cursor2 = colName[.entries].find({"type": "status", "info.state_code" : code})
+                            for doc2 in 
+
+                    if cursor.alive():
+                        cursor = cursor.next()
+                    else:
+                        break
+        elif:
+            continue
     return -1
