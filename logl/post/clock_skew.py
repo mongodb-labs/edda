f5c44b18c297d5d049720ac6aa8d1c0960c2b342
Samantha Ritter <samantha.ritter@10gen.com>
2012-06-19 12:17:05 -0400

    Debugging with tests

--- logl/post/clock_skew.py
+++ logl/post/clock_skew.py
@@ -54,56 +54,56 @@ def detect(a, b, db, collName):
     and return it as a timedelta object.  If
     unable to detect skew, return None.  This is different
     from 0 skew, which will be a timedelta with value 0"""
-    cursor_a = collName["entries"].find({"type" : "status", "origin_server" : a, "info.server" : b})
-    cursor_b = collName["entries"].find({"type" : "Status", "origin_server" : b, "info.server" : b})
+    entries_a = db[collName + ".entries"]
+    entries_b = db[collName + ".entries"]
+    cursor_a = entries_a.find({"type" : "status", "origin_server" : a, "info.server" : b})
+    cursor_b = entries_b.find({"type" : "status", "origin_server" : b, "info.server" : "self"})
     cursor_a.sort("date")
     cursor_b.sort("date")
     logger = logging.getLogger(__name__)
     logger.debug("Detecting clock skew for pair {0} - {1}...".format(a, b))
-    if not cursor_a.alive():
+    try:
+        a_1 = cursor_a.next()
+        b_1 = cursor_b.next()
+    except StopIteration:
+        logger.debug("Cursors are empty!  No entries about these servers")
         return None
-    if not cursor_b.alive():
-        return None
-    a_1 = cursor_a.next()
-    b_1 = cursor_b.next()
     min_time = timedelta(seconds=2)
     # take and compare two consecutive entries from each cursor
     while True:
-        logger.debug("Using next set of entries")
-        if not cursor_a.alive():
-            return None
-        if not cursor_b.alive():
-            return None
-        a_2 = cursor_a.next()
-        b_2 = cursor_b.next()
-        # if first entries do not match, advance A
-        while a_1["state_code"] != b_1["state_code"]:
-            logger.debug("first entries do not match")
-            a_1 = a_2
-            if not cursor_a.alive():
-                return None
-            a_2 = cursor_a.next()
-        # if first entries match but not second ones, advance A and B
-        if (a_1["state_code"] == b_1["state_code"]) and (a_2["state_code"] != b_2["state_code"]):
-            logger.debug("first entries match, but not second ones")
+        try:
+            logger.debug("Using next set of entries")
             a_2 = cursor_a.next()
             b_2 = cursor_b.next()
-        # if both first and second entries match, take clock skew
-        # (fix me so I work better please...)
-        if (a_1["info"]["state_code"] == b_1["info"]["state_code"]) and (a_2["info"]["state"] == b_2["info"]["state"]):
-            logger.debug("Both entries match!  Calculating clock skew")
-            td1 = a_1["date"] - b_1["date"]
-            td2 = a_2["date"] - b_2["date"]
-            # if td1 and td2 are wildly different, try again
-            diff = td1 - td2
-            if abs(diff) < min_time:
-                logger.debug("td1 and td2 agree.  Big enough for clock skew?")
+            # if first entries do not match, advance A
+            while a_1["info"]["state_code"] != b_1["info"]["state_code"]:
+                logger.debug("first entries do not match")
+                a_1 = a_2
+                a_2 = cursor_a.next()
+            # if first entries match but not second ones, advance A and B
+            if (a_1["info"]["state_code"] == b_1["info"]["state_code"]) and (a_2["info"]["state_code"] != b_2["info"]["state_code"]):
+                logger.debug("first entries match, but not second ones")
+                a_2 = cursor_a.next()
+                b_2 = cursor_b.next()
+            # if both first and second entries match, take clock skew
+            # (fix me so I work better please...)
+            if (a_1["info"]["state_code"] == b_1["info"]["state_code"]) and (a_2["info"]["state"] == b_2["info"]["state"]):
+                logger.debug("Both entries match!  Calculating clock skew")
+                td1 = a_1["date"] - b_1["date"]
+                td2 = a_2["date"] - b_2["date"]
+                # if td1 and td2 are wildly different, try again
+                diff = td1 - td2
+                if abs(diff) < min_time:
+                    logger.debug("td1 and td2 agree.  Big enough for clock skew?")
                 # they agree.  But big enough for clock skew?
                 if abs(td1) > min_time:
                     logger.debug("clock skew found!  Returning {0}".format(td1))
                     return td1 # or return the smaller of the two?
-        a_1 = a_2
-        b_1 = b_2
+            a_1 = a_2
+            b_1 = b_2
+        except StopIteration:
+            logger.debug("Ran out of entries, could not detect clock skew")
+            return None
 
 
 def clock_skew_doc(name):
--- test/test_clock_skew.py
+++ test/test_clock_skew.py
@@ -1,4 +1,4 @@
-from logl.post import *
+from logl.post.clock_skew import *
 from logl.logl import new_server
 import pymongo
 from datetime import datetime
@@ -12,9 +12,9 @@ def test_server_clock_skew():
 def test_detect_simple():
     """A simple test of the detect() method in post.py"""
     c = Connection()
-    db = c[test]
-    servers = db[test.servers]
-    entries = db[test.entries]
+    db = c["test"]
+    servers = db["wildcats.servers"]
+    entries = db["wildcats.entries"]
     # fill in some servers
     servers.insert(new_server(1, "Erica"))
     servers.insert(new_server(2, "Alison"))
@@ -29,11 +29,17 @@ def test_detect_simple():
     entries.insert(generate_doc("status", "Alison", "SECONDARY", 2, "self", datetime.now()))
     entries.insert(generate_doc("status", "Alison", "PRIMARY", 1, "self", datetime.now()))
     # run detect()!
-    assert detect("Erica", "Alison", c, test)
-    pass
+    t1 = detect("Erica", "Alison", db, "wildcats")
+    assert t1
+    assert abs(abs(t1) - timedelta(seconds=10)) < timedelta(seconds=2)
+    t2 = detect("Alison", "Erica", db, "wildcats")
+    assert t2
+    assert abs(abs(t2) - timedelta(seconds=10)) < timedelta(seconds=2)
+    assert abs(t1) == abs(t2)
+    # once sign convention is established:
+    # assert t1 == -t2
 
-
-def generate_entry(type, server, label, code, target, date):
+def generate_doc(type, server, label, code, target, date):
     """Generate an entry"""
     doc = {}
     doc["type"] = type
@@ -49,11 +55,10 @@ def generate_entry(type, server, label, code, target, date):
 def test_clock_skew_doc():
     """Simple tests of the clock_skew_doc() method
     in post.py"""
-    doc = clock_skew_def("Samantha")
+    doc = clock_skew_doc("Samantha")
     assert doc
     assert doc["server_name"] == "Samantha"
     assert doc["type"] == "clock_skew"
-    assert doc["partners"]
     pass
 
 
