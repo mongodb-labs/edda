2cded8976f415eed3f32061d98d2eaab79bd94f1
Samantha Ritter <samantha.ritter@10gen.com>
2012-06-26 10:42:12 -0400

    Add arrow heads

--- logl/display/js/arrows.js
+++ logl/display/js/arrows.js
@@ -12,16 +12,37 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
+arrow = function() {
+    canvas = document.getElementById("arrow_layer");
+    ctx = canvas.getContext("2d");
+    one_arrow(400, 400, 500, 500, ctx);
+    one_arrow(90, 4, 124, 543, ctx);
+    one_arrow(100, 100, 300, 300, ctx);
+    one_arrow(300, 300, 100, 100, ctx);
+};
+
 
 one_arrow = function(x1, y1, x2, y2, ctx) {
 
     dx = x2 - x1;
     dy = y2 - y1;
 
-    cx = x + dx/2 + dy/4;
-    cy = y + dy/2 + dx/4;
+    cx = x1 + dx/2 - dy/4;
+    cy = y1 + dy/2 + dx/4;
 
     ctx.moveTo(x1, y1);
+    ctx.lineWidth = 3;
     ctx.quadraticCurveTo(cx, cy, x2, y2);
     ctx.stroke();
+
+    // adapted from http://stackoverflow.com/questions/808826/draw-arrow-on-canvas-tag
+    var headlen = 20;   // length of head in pixels
+    var angle = Math.atan2(y2 - y1,x2 - x1) - Math.PI/8;
+    ctx.beginPath();
+    ctx.moveTo(x2, y2);
+    ctx.lineTo(x2-headlen*Math.cos(angle-Math.PI/8),y2-headlen*Math.sin(angle-Math.PI/8));
+    ctx.lineTo(x2-headlen*Math.cos(angle+Math.PI/8),y2-headlen*Math.sin(angle+Math.PI/8));
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.fill();
 };
