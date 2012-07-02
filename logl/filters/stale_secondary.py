882506beb02ed4cb6fd4f43e4af8d85fe8e382ef
Kaushal Parikh <kaushal.parikh@10gen.com>
2012-07-02 15:48:00 -0400

    Added filter for secondary becoming "STALE", and the tests to go along with it.

--- logl/filters/stale_secondary.py
+++ logl/filters/stale_secondary.py
@@ -11,3 +11,78 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# This module tracks requests from the server to lock or unlock its self from writes. 
+
+
+import re
+import string
+import logging
+from ..parse_ip import *
+
+# Mon Jul  2 10:00:11 [conn2] CMD fsync: sync:1 lock:1
+# Mon Jul  2 10:00:04 [conn2] command: unlock requested
+# Mon Jul  2 10:00:10 [conn2] db is now locked for snapshotting, no writes allowed. db.fsyncUnlock() to unlock
+
+def criteria(msg):
+    """Does the given log line fit the criteria for this filter?
+    return an integer code if yes, -1 if not."""
+    if string.find(msg, 'too stale to catch up') >= 0:
+        return 0
+    return -1
+
+
+def process(msg, date):
+    """if the given log line fits the criteria for this filter,
+    processes the line and creates a document for it.
+    document = {
+       "date" : date,
+       "type" : "conn",
+       "info" : {
+          "state_code" : messagetype
+          "state" : "STALE"
+          "server" : host:port
+       }
+       "oritinal_message" : msg 
+    }"""
+    message_type = criteria(msg)
+    if message_type < 0:
+        return None
+
+    doc = {}
+    doc["date"] = date
+    doc["type"] = "stale"
+    doc["info"] = {}
+    doc["info"]["state_code"] = message_type
+    doc["original_message"] = msg
+
+    ip = True
+    if message_type == 0:
+        doc["info"]["state"] = "STALE"
+        logger = logging.getLogger(__name__)
+
+        # this very long regex recognizes legal IP addresses
+        pattern = re.compile("(([0|1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))(\.([0|1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5])){3}")
+        m = pattern.search(msg)
+        if (m == None):
+            logger.debug("malformed new_conn message: no IP address found")
+            ip = False
+        if ip:
+            host = m.group(0)
+
+            # isolate port number
+            pattern = re.compile(":[0-9]{1,5}")
+            n = pattern.search(msg[21:])
+            if n is None:
+                logger.debug("malformed new_conn message: no port number found")
+                ip = false
+            port = n.group(0)[1:]
+        if ip:
+            doc["info"]["server"] = host + ":" + port
+        else:
+            doc["info"]["server"] = None
+        #start = string.find(msg, " sync:")
+        #doc["info"]["sync_num"] = int(msg[start + 6: start + 7])
+        #start = string.find(msg, "lock:")
+        #doc["info"]["lock_num"] = int(msg[start + 5: start + 6])
+    return doc
---
+++ test/test_stale_secondary.py
@@ -0,0 +1,50 @@
+# Copyright 2012 10gen, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from logl.filters.stale_secondary import *
+from datetime import datetime
+
+
+# Mon Jul  2 10:00:11 [conn2] CMD fsync: sync:1 lock:1
+# Mon Jul  2 10:00:04 [conn2] command: unlock requested
+# Mon Jul  2 10:00:10 [conn2] db is now locked for snapshotting, no writes allowed. db.fsyncUnlock() to unlock
+def test_criteria():
+    assert criteria("this should not pass") == -1
+    assert criteria("Thu Sep 9 17:22:46 [rs_sync] replSet error RS102 too stale to catch up") == 0
+    assert criteria("Thu Sep 9 17:24:46 [rs_sync] replSet error RS102 too stale to catch up, at least from primary: 127.0.0.1:30000") == 0
+
+
+def test_process():
+    date = datetime.now()
+    check_state("Thu Sep 9 17:22:46 [rs_sync] replSet error RS102 too stale to catch up", 0, date, None)
+    check_state("Thu Sep 9 17:24:46 [rs_sync] replSet error RS102 too stale to catch up, at least from primary: 127.0.0.1:30000", 0, date, "127.0.0.1:30000")
+    assert process("This should fail", date) == None
+
+
+def check_state(message, code, date, server):
+    doc = process(message, date)
+    assert doc
+    assert doc["type"] == "stale"
+    assert doc["original_message"] == message
+    assert doc["info"]["state_code"] == code
+    if server != None:
+        print "server:     *{}*".format(server)
+        print "comparison: *{}*".format(doc["info"]["server"])
+        assert doc["info"]["server"] == server
+    #if sync != 0:
+    #	print "Sync Num: {}".format(doc["info"]["sync_num"])
+    # 	assert doc["info"]["sync_num"] == sync
+    #	assert doc["info"]["lock_num"] == lock
+
+    #print 'Server number is: *{0}*, testing against, *{1}*'.format(doc["info"]["server"], server)
