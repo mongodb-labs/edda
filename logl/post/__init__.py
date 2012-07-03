58696fd79fd594d24fcf4f71704a05e676afecf1
Samantha Ritter <samantha.ritter@10gen.com>
2012-07-03 11:26:28 -0400

    In a failed attempt to fix importing problem, add logic to __init__.py

--- logl/post/__init__.py
+++ logl/post/__init__.py
@@ -11,3 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import os
+import re
+
+__all__ = []
+dirList = os.listdir(os.path.dirname(os.path.abspath(__file__)))
+pattern = re.compile(".py$")
+
+for d in dirList:
+    # ignore anything that isn't strictly .py
+    m = pattern.search(d)
+    if (m != None):
+        d = d[0:len(d) - 3]
+        __all__.append(d)
+
