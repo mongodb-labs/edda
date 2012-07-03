a397f3450edbb98887d199c6fdb289ffd890947f
Samantha Ritter <samantha.ritter@10gen.com>
2012-07-03 12:22:10 -0400

    Tests for connection.py

--- test/test_connection.py
+++ test/test_connection.py
@@ -15,10 +15,46 @@
 from logl.post.connection import *
 import os
 from nose.plugins.skip import Skip, SkipTest
+from datetime import datetime
+from copy import deepcopy
 
 def test_send_to_js():
     """Test the send_to_js() method"""
-    raise SkipTest
-    send_to_js("Hey there from Python!")
+#    raise SkipTest
+    send_to_js(generate_msg())
 
 
+def generate_msg():
+    # these are sample frames, just for testing
+    frames = {}
+    frame = {}
+    # first frame
+    frame["time"] = str(datetime.now())
+    frame["server_count"] = 4
+    frame["user_count"] = 1
+    frame["flag"] = False
+    frame["servers"] = {}
+    frame["users"] = {}
+    frame["servers"]["samantha@home:27017"] = "PRIMARY"
+    frame["servers"]["matthew@home:45234"] = "SECONDARY"
+    frame["servers"]["stewart@home:00981"] = "SECONDARY"
+    frame["servers"]["carin@home:27017"] = "ARBITER"
+    frame["users"]["benjamin@home:10098"] = "USER"
+    frame["syncs"] = {}
+    frame["syncs"]["samantha@home:27017"] = "matthew@home:45234"
+    frame["connections"] = {}
+    frame["connections"]["benjamin@home:10098"] = "samantha@home:27017"
+    # second frame
+    frame1 = deepcopy(frame)
+    frame2 = deepcopy(frame)
+    frame3 = deepcopy(frame)
+    frame1["servers"]["samantha@home:27017"] = "DOWN"
+    frame2["servers"]["matthew@home:45234"] = "PRIMARY"
+    frame2["servers"]["samantha@home:27017"] = "DOWN"
+    frame3["servers"]["samantha@home:27017"] = "SECONDARY"
+    frame3["servers"]["matthew@home:45234"] = "PRIMARY"
+    frames["0"] = frame
+    frames["1"] = frame1
+    frames["2"] = frame2
+    frames["3"] = frame3
+    return frames
