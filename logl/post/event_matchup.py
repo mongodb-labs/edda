e7d1326dd9fb5a5f21766b3ebbe75ed76311e17a
Samantha Ritter <samantha.ritter@10gen.com>
2012-07-05 11:38:44 -0400

    Write basic outline for event matchup

---
+++ logl/post/event_matchup.py
@@ -0,0 +1,61 @@
+# Copyright 2012 10gen, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/usr/bin/env python
+
+import pymongo
+
+def event_matchup(db, collName):
+    """This method sorts through the db's entries to
+    find discrete events that happen across servers.  It will
+    organize these events into a list of "events", which are
+    each a dictionary built as follows:
+    event = {
+        "type"       = type of event, see list below
+        "date"       = datetime, as string
+        "target"     = affected server
+        "dissenters" = servers who did not see this event
+                       (not for connection or sync messages)
+        "summary"    = a mnemonic summary of the event
+    (event-specific additional fields:)
+        "sync_to"    = for sync type messages only
+        "conn_IP"    = for new_conn or end_conn messages only
+        "conn_num"   = for new_conn or end_conn messages only
+        "state"      = for status type messages only (label, not code)
+        }
+
+    possible event types include:
+    "new_conn" : new user connections
+    "end_conn" : end a user connection
+    "status"   : a status change for a server
+    "sync"     : a new sync pattern for a server
+    "stale"    : a secondary is going stale
+    "exit"     : a replica set is exiting
+    "lock"     : a server requests to lock itself from writes
+    "unlock"   : a server requests to unlock itself from writes
+    "reconfig" : new config information was received
+
+    This module assumes that normal network delay can account
+    for up to 4 seconds of lag between server logs.  Beyond this margin,
+    module assumes that servers are no longer in sync.
+    """
+    pass
+
+
+def generate_summary(event):
+    """Given an event, generates and returns a one-line,
+    mnemonic summary for that event"""
+    pass
+
+
