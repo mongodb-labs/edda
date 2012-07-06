f20350d1357c95ad647cda5e6017267c101ec3db
Samantha Ritter <samantha.ritter@10gen.com>
2012-07-06 11:03:01 -0400

    Begin to make arrows straight instead of bowed; changed angle of arrowheads

--- logl/display/js/arrows.js
+++ logl/display/js/arrows.js
@@ -12,15 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-arrow = function() {
-    canvas = document.getElementById("arrow_layer");
-    ctx = canvas.getContext("2d");
-    one_arrow(400, 400, 500, 500, ctx);
-    one_arrow(90, 4, 124, 543, ctx);
-    one_arrow(100, 100, 300, 300, ctx);
-    one_arrow(300, 300, 100, 100, ctx);
-};
-
 
 one_arrow = function(x1, y1, x2, y2, ctx) {
 
@@ -37,11 +28,11 @@ one_arrow = function(x1, y1, x2, y2, ctx) {
 
     // adapted from http://stackoverflow.com/questions/808826/draw-arrow-on-canvas-tag
     var headlen = 20;   // length of head in pixels
-    var angle = Math.atan2(y2 - y1,x2 - x1) - Math.PI/8;
+    var angle = Math.atan2(y2 - y1,x2 - x1);
     ctx.beginPath();
     ctx.moveTo(x2, y2);
-    ctx.lineTo(x2-headlen*Math.cos(angle-Math.PI/8),y2-headlen*Math.sin(angle-Math.PI/8));
-    ctx.lineTo(x2-headlen*Math.cos(angle+Math.PI/8),y2-headlen*Math.sin(angle+Math.PI/8));
+    ctx.lineTo(x2-headlen*Math.cos(angle),y2-headlen*Math.sin(angle));
+    ctx.lineTo(x2-headlen*Math.cos(angle),y2-headlen*Math.sin(angle));
     ctx.lineTo(x2, y2);
     ctx.stroke();
     ctx.fill();
