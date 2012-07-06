fe0b6748702489041e81a1387f036e745e64157d
Samantha Ritter <samantha.ritter@10gen.com>
2012-07-06 14:43:07 -0400

    Group all global variables and setup functions into one, centralized document

--- logl/display/js/setup.js
+++ logl/display/js/setup.js
@@ -11,3 +11,98 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// A centralized file for all the JS setup details
+
+// define program-wide global variables
+var layers = new Array("background", "shadow", "connection", "arrow", "server", "text", "message");
+var canvases = {};
+var contexts = {};
+var servers = {};
+var frames = {};
+var slider = {};
+
+
+// call various setup functions
+function logl_setup() {
+    canvases_and_contexts();
+    mouse_over_setup();
+    connect();  // see connection.js
+    time_setup(frames.size);
+    visual_setup();
+};
+
+// set up canvases and associated contexts
+function canvases_and_contexts() {
+    for (var i in layers) {
+	canvases[layers[i]] = document.getElementById(layers[i] + "_layer");
+	contexts[layers[i]] = canvases[layers[i]].getContext("2d");
+    }
+};
+
+
+// set up mouse-over functionality
+function mouse_over_setup() {
+    canvases["shadow"].addEventListener("mousemove", on_canvas_mouseover, false);
+};
+
+
+// set up display-related things from frames
+// generate coordinates
+// set background to brown
+function visual_setup() {
+    var names = new Array();
+    if (frames) {
+	if (frames["0"]) {
+	    for (var name in frames["0"]["servers"]) {
+		names.push(name);
+	    }
+	    generate_coords(names.length, names);
+	}
+    }
+
+    // clear all layers
+    for (var name in layers) {
+	canvases[layers[name]].width = canvases[layers[name]].width;
+    }
+
+    // color background
+    var b_cvs = canvases["background"];
+    var b_ctx = contexts["background"];
+    b_ctx.beginPath();
+    b_ctx.rect(0, 0, b_cvs.width, b_cvs.height);
+    b_ctx.fillStyle = "#4E3629";
+    b_ctx.fill();
+
+    // render first frame
+    render("0");
+
+};
+
+
+// set up slider functionality
+function time_setup(max_time) {
+    $("#slider").slider({ slide: function(event, ui) {
+		render(ui.value);
+		document.getElementById("timestamp").innerHTML = frames[ui.value]["date"];
+	    }});
+    $("#slider").slider( "option", "min", 0 );
+    $("#slider").slider( "option", "max", max_time );
+    $("#slider").slider( "option", "step", 1 );
+};
+
+
+// define .size function for object
+// thanks http://stackoverflow.com/questions/5223/length-of-javascript-object-ie-associative-array
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+	if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+
+
+
+
