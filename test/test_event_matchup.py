1f172c7ffd07243f40399fecd9db4cc8cd3f53c8
Samantha Ritter <samantha.ritter@10gen.com>
2012-07-09 14:38:31 -0400

    Add tests for the next_event() method of event_matchup()

--- test/test_event_matchup.py
+++ test/test_event_matchup.py
@@ -14,22 +14,37 @@
 
 # testing file for logl/post/event_matchup.py
 
-
+import logging
 from pymongo import Connection
 from logl.logl import assign_address
 from logl.post.event_matchup import *
 import pymongo
 from datetime import datetime
-
+from datetime import timedelta
 
 def db_setup():
     """Set up necessary server connections"""
     c = Connection()
+    logging.basicConfig(level=logging.DEBUG)
     db = c["test_event_matchup"]
     servers = db["AdventureTime.servers"]
     entries = db["AdventureTime.entries"]
     db.drop_collection(servers)
     db.drop_collection(entries)
+    return [servers, entries, db]
+
+
+def db_setup_n_servers(n):
+    """Set up necessary server connection, and
+    add n servers to the .servers collection.  Set
+    the server_num to an int i < n, set the IP
+    field to i.i.i.i, and the hostname to i@10gen.com"""
+    servers, entries, db = db_setup()
+    for i in range(1,n):
+        ip = str(i) + "." + str(i) + "." + str(i) + "." + str(i)
+        hostname = str(i) + "@10gen.com"
+        assign_address(i, ip, servers)
+        assign_address(i, hostname, servers)
     return [servers, entries, db]
 
 
@@ -41,8 +56,13 @@ def generate_entries(x, y):
     return [a, b]
 
 
-def one_entry(type, )
-    """Generate an entry with the specified fields"""
+def one_entry(type, o_s, date, info):
+    """Generate an entry with the specified type and origin_server"""
+    e = {}
+    e["type"] = type
+    e["origin_server"] = o_s
+    e["date"] = date
+    e["info"] = info
     return e
 
 
@@ -55,34 +75,322 @@ def test_event_matchup_empty():
 # test the next_event() method
 # -----------------------------
 
-def test_next_event_one_server():
+
+def test_next_event_one_server_state_msg():
     """Test next_event() on lists from just one server"""
-    pass
+    servers, entries, db = db_setup_n_servers(1)
+    server_nums = {"1"}
+    server_entries = {}
+    server_entries["1"] = []
+    info = {}
+    info["state"] = "ARBITER"
+    info["state_code"] = 7
+    info["server"] = "34@10gen.com:34343"
+    date = datetime.now()
+    e1 = one_entry("status", "1", date, info)
+    server_entries["1"].append(e1)
+    event = next_event(server_nums, server_entries, db, "AdventureTime")
+    assert event
+    assert event["witnesses"]
+    assert len(event["witnesses"]) == 1
+    assert not event["dissenters"]
+    assert event["type"] == "status"
+    assert event["date"] == date
+    assert event["state"] == "ARBITER"
+    assert event["target"] == "34@10gen.com:34343"
+    assert event["summary"] == "Server 34@10gen.com:34343 is now ARBITER"
+
+
+def test_next_event_two_servers():
+    """Test next_event() on two servers with matching entries"""
+    servers, entries, db = db_setup_n_servers(2)
+    server_nums = {"1", "2"}
+    server_entries = {}
+    server_entries["1"] = []
+    server_entries["2"] = []
+    info = {}
+    info["state"] = "SECONDARY"
+    info["state_code"] = 2
+    info["server"] = "llama@the.zoo"
+    e1 = one_entry("status", "1", datetime.now(), info)
+    e2 = one_entry("status", "2", datetime.now(), info)
+    server_entries["1"].append(e1)
+    server_entries["2"].append(e2)
+    # run next_event()
+    event = next_event(server_nums, server_entries, db, "AdventureTime")
+    assert event
+    assert event["witnesses"]
+    assert len(event["witnesses"]) == 2
+    assert "1" in event["witnesses"]
+    assert "2" in event["witnesses"]
+    assert not event["dissenters"]
+    assert event["type"] == "status"
+    assert event["state"] == "SECONDARY"
+    assert event["target"] == "llama@the.zoo"
+    assert event["summary"] == "Server llama@the.zoo is now SECONDARY"
+
+
+def test_next_event_four_servers():
+    """Test next_event() on four servers with matching entries"""
+    servers, entries, db = db_setup_n_servers(4)
+    server_nums = {"1", "2", "3", "4"}
+    server_entries = {}
+    server_entries["1"] = []
+    server_entries["2"] = []
+    server_entries["3"] = []
+    server_entries["4"] = []
+    info = {}
+    info["state"] = "SECONDARY"
+    info["state_code"] = 2
+    info["server"] = "llama@the.zoo"
+    e1 = one_entry("status", "1", datetime.now(), info)
+    e2 = one_entry("status", "2", datetime.now(), info)
+    e3 = one_entry("status", "3", datetime.now(), info)
+    e4 = one_entry("status", "4", datetime.now(), info)
+    server_entries["1"].append(e1)
+    server_entries["2"].append(e2)
+    server_entries["3"].append(e3)
+    server_entries["4"].append(e4)
+    # run next_event()
+    event = next_event(server_nums, server_entries, db, "AdventureTime")
+    assert event
+    assert event["witnesses"]
+    assert len(event["witnesses"]) == 4
+    assert "1" in event["witnesses"]
+    assert "2" in event["witnesses"]
+    assert "3" in event["witnesses"]
+    assert "4" in event["witnesses"]
+    assert not event["dissenters"]
+    assert event["type"] == "status"
+    assert event["state"] == "SECONDARY"
+    assert event["target"] == "llama@the.zoo"
+    assert event["summary"] == "Server llama@the.zoo is now SECONDARY"
+
+
+def test_next_event_two_servers_no_match():
+    """Test next_event() on two servers with non-matching entries"""
+    servers, entries, db = db_setup_n_servers(2)
+    server_nums = {"1", "2"}
+    server_entries = {}
+    server_entries["1"] = []
+    server_entries["2"] = []
+    info1, info2 = {}, {}
+    info1["state"], info2["state"] = "PRIMARY", "SECONDARY"
+    info1["state_code"], info2["state_code"] = 1, 2
+    info1["server"], info2["server"] = "llama@the.zoo", "llama@the.zoo"
+    e1 = one_entry("status", "1", datetime.now(), info1)
+    e2 = one_entry("status", "2", datetime.now(), info2)
+    server_entries["1"].append(e1)
+    server_entries["2"].append(e2)
+    # run next_event()
+    event1 = next_event(server_nums, server_entries, db, "AdventureTime")
+    event2 = next_event(server_nums, server_entries, db, "AdventureTime")
+    assert event1
+    assert event2
+    assert event1["witnesses"]
+    assert event2["witnesses"]
+    assert event1["dissenters"]
+    assert event2["dissenters"]
+    assert len(event1["witnesses"]) == 1
+    assert len(event2["witnesses"]) == 1
+    assert len(event1["dissenters"]) == 1
+    assert len(event2["dissenters"]) == 1
+    assert "1" in event1["witnesses"]
+    assert "2" in event2["witnesses"]
+    assert "1" in event2["dissenters"]
+    assert "2" in event1["dissenters"]
+    assert event1["state"] == "PRIMARY"
+    assert event2["state"] == "SECONDARY"
+    assert event1["target"] == event2["target"]
+
+
+def test_next_event_two_servers_lag():
+    """Test next_event() on two servers with lag greater than
+    allowable network delay"""
+    servers, entries, db = db_setup_n_servers(2)
+    server_nums = {"1", "2"}
+    server_entries = {}
+    server_entries["1"] = []
+    server_entries["2"] = []
+    info = {}
+    info["state"] = "ARBITER"
+    info["state_code"] = 7
+    info["server"] = "sam@10gen.com"
+    e1 = one_entry("status", "1", datetime.now(), info)
+    e2 = one_entry("status", "2", datetime.now() + timedelta(seconds=4), info)
+    server_entries["1"].append(e1)
+    server_entries["2"].append(e2)
+    # run next_event()
+    event1 = next_event(server_nums, server_entries, db, "AdventureTime")
+    event2 = next_event(server_nums, server_entries, db, "AdventureTime")
+    assert event1
+    assert event2
+    assert event1["witnesses"]
+    assert event2["witnesses"]
+    assert event1["dissenters"]
+    assert event2["dissenters"]
+    assert len(event1["witnesses"]) == 1
+    assert len(event2["witnesses"]) == 1
+    assert len(event1["dissenters"]) == 1
+    assert len(event2["dissenters"]) == 1
+    assert "1" in event1["witnesses"]
+    assert "2" in event2["witnesses"]
+    assert "1" in event2["dissenters"]
+    assert "2" in event1["dissenters"]
+
+
+def test_next_event_three_servers_one_lag():
+    """Test next_event() on three servers with lag greater than
+    allowable network delay affecting one server"""
+    servers, entries, db = db_setup_n_servers(3)
+    server_nums = {"1", "2", "3"}
+    server_entries = {}
+    server_entries["1"] = []
+    server_entries["2"] = []
+    server_entries["3"] = []
+    info = {}
+    info["state"] = "ARBITER"
+    info["state_code"] = 7
+    info["server"] = "sam@10gen.com"
+    e1 = one_entry("status", "1", datetime.now(), info)
+    e2 = one_entry("status", "2", datetime.now() + timedelta(seconds=4), info)
+    e3 = one_entry("status", "3", datetime.now(), info)
+    server_entries["1"].append(e1)
+    server_entries["2"].append(e2)
+    server_entries["3"].append(e3)
+    # run next_event()
+    event1 = next_event(server_nums, server_entries, db, "AdventureTime")
+    event2 = next_event(server_nums, server_entries, db, "AdventureTime")
+    assert not next_event(server_nums, server_entries, db, "AdventureTime")
+    assert event1
+    assert event2
+    assert event1["witnesses"]
+    assert event2["witnesses"]
+    assert event1["dissenters"]
+    assert event2["dissenters"]
+    assert len(event1["witnesses"]) == 2
+    assert len(event2["witnesses"]) == 1
+    assert len(event1["dissenters"]) == 1
+    assert len(event2["dissenters"]) == 2
+    assert "1" in event1["witnesses"]
+    assert "3" in event1["witnesses"]
+    assert "2" in event2["witnesses"]
+    assert "3" in event2["dissenters"]
+    assert "1" in event2["dissenters"]
+    assert "2" in event1["dissenters"]
+
+
+def test_next_event_no_entries():
+    """Test next_event() for an event where there
+    are no entries for a certain server.  So,
+    server_entries["name"] is None"""
+    servers, entries, db = db_setup_n_servers(2)
+    server_nums = {"1", "2"}
+    server_entries = {}
+    server_entries["1"] = []
+    server_entries["2"] = []
+    info = {}
+    info["state"] = "SECONDARY"
+    info["state_code"] = 2
+    info["server"] = "llama@the.zoo"
+    e1 = one_entry("status", "1", datetime.now(), info)
+    server_entries["1"].append(e1)
+    # run next_event()
+    event = next_event(server_nums, server_entries, db, "AdventureTime")
+    assert event
+    assert event["witnesses"]
+    assert len(event["witnesses"]) == 1
+    assert "1" in event["witnesses"]
+    assert event["dissenters"]
+    assert len(event["dissenters"]) == 1
+    assert "2" in event["dissenters"]
 
 
 def test_next_event_all_empty():
     """Test next_event() on all empty lists, but with
     server names present (i.e. we ran out of entries)"""
-    pass
+    servers, entries, db = db_setup_n_servers(2)
+    server_nums = {"1", "2"}
+    server_entries = {}
+    server_entries["1"] = []
+    server_entries["2"] = []
+    # run next_event()
+    assert not next_event(server_nums, server_entries, db, "AdventureTime")
 
 
 def test_next_event_all_empty_but_one():
     """Test next_event() on input on many servers, but with
     all servers' lists empty save one (i.e. we went through
     the other servers' entries already)"""
-    pass
+    """Test next_event() for an event where there
+    are no entries for a certain server.  So,
+    server_entries["name"] is None"""
+    servers, entries, db = db_setup_n_servers(4)
+    server_nums = {"1", "2", "3", "4"}
+    server_entries = {}
+    server_entries["1"] = []
+    server_entries["2"] = []
+    server_entries["3"] = []
+    server_entries["4"] = []
+    info = {}
+    info["state"] = "SECONDARY"
+    info["state_code"] = 2
+    info["server"] = "llama@the.zoo"
+    e1 = one_entry("status", "1", datetime.now(), info)
+    server_entries["1"].append(e1)
+    # run next_event()
+    event = next_event(server_nums, server_entries, db, "AdventureTime")
+    assert event
+    assert event["witnesses"]
+    assert len(event["witnesses"]) == 1
+    assert "1" in event["witnesses"]
+    assert event["dissenters"]
+    assert len(event["dissenters"]) == 3
+    assert "2" in event["dissenters"]
+    assert "3" in event["dissenters"]
+    assert "4" in event["dissenters"]
 
 
-def test_next_event_one():
+def test_next_event_two_matching_some_lag():
     """Test next_event() on lists from two servers
-    with one matching entry in each server's list"""
-    pass
-
-
-def test_next_event_two():
-    """Test next_event() on lists from two servers
-    with entries that do not match"""
-    pass
+    with entries that do match, but are a second apart in time"""
+    servers, entries, db = db_setup_n_servers(2)
+    server_nums = {"1", "2"}
+    server_entries = {}
+    server_entries["1"] = []
+    server_entries["2"] = []
+    info = {}
+    info["state"] = "SECONDARY"
+    info["state_code"] = 2
+    info["server"] = "llama@the.zoo"
+    e1 = one_entry("status", "1", datetime.now(), info)
+    e2 = one_entry("status", "2", datetime.now() + timedelta(seconds=1), info)
+    server_entries["2"].append(e2)
+    server_entries["1"].append(e1)
+    info2 = {}
+    info2["state"] = "FATAL"
+    info2["state_code"] = 4
+    info2["server"] = "finn@adventure.time"
+    e3 = one_entry("status", "1", datetime.now(), info2)
+    e4 = one_entry("status", "2", datetime.now() + timedelta(seconds=1), info2)
+    server_entries["1"].append(e3)
+    server_entries["2"].append(e4)
+    # run next_event()
+    event = next_event(server_nums, server_entries, db, "AdventureTime")
+    event2 = next_event(server_nums, server_entries, db, "AdventureTime")
+    assert not next_event(server_nums, server_entries, db, "AdventureTime")
+    assert event
+    assert event2
+    assert event["witnesses"]
+    assert event2["witnesses"]
+    assert len(event["witnesses"]) == 2
+    assert len(event2["witnesses"]) == 2
+    assert "1" in event["witnesses"]
+    assert "2" in event["witnesses"]
+    assert "1" in event2["witnesses"]
+    assert "2" in event2["witnesses"]
+    assert not event["dissenters"]
+    assert not event2["dissenters"]
 
 
 # -------------------------------------
@@ -111,7 +419,7 @@ def test_target_server_match_both_same_hostname():
     info.server fields, using hostnames"""
     servers, entries, db = db_setup()
     a, b = generate_entries("sam@10gen.com", "sam@10gen.com")
-    assert target_server_match(a, b, servers)
+    assert target_server_match(a, b, servers) == "sam@10gen.com"
 
 
 def test_target_server_match_both_different_hostnames():
@@ -147,7 +455,7 @@ def test_target_server_match_IP():
     b["origin_server"] = "2"
     assign_address(1, "1.1.1.1", servers)
     assign_address(2, "2.2.2.2", servers)
-    assert target_server_match(a, b, servers)
+    assert target_server_match(a, b, servers) == "1.1.1.1"
 
 
 def test_target_server_match_hostname():
@@ -159,7 +467,7 @@ def test_target_server_match_hostname():
     b["origin_server"] = "2"
     assign_address(1, "finn@adventure.time", servers)
     assign_address(2, "jake@adventure.time", servers)
-    assert target_server_match(a, b, servers)
+    assert target_server_match(a, b, servers) == "jake@adventure.time"
 
 
 def test_target_server_match_IP_no_match():
@@ -196,7 +504,7 @@ def test_target_server_match_unknown_IP():
     b["origin_server"] = "2"
     assign_address(1, "unknown", servers)
     assign_address(2, "2.2.2.2", servers)
-    assert target_server_match(a, b, servers)
+    assert target_server_match(a, b, servers) == "1.1.1.1"
 
 
 def test_target_server_match_unknown_hostname():
@@ -209,7 +517,7 @@ def test_target_server_match_unknown_hostname():
     b["origin_server"] = "2"
     assign_address(1, "LSP@adventure.time", servers)
     assign_address(2, "unknown", servers)
-    assert target_server_match(a, b, servers)
+    assert target_server_match(a, b, servers) == "treetrunks@adventure.time"
 
 
 # -------------------------------------
