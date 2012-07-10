44cb6d89071db66548d5a62e0cc063888e46f905
Samantha Ritter <samantha.ritter@10gen.com>
2012-07-10 11:49:43 -0400

    Add tests for frames.py

--- test/test_frames.py
+++ test/test_frames.py
@@ -15,30 +15,108 @@
 from logl.post.frames import *
 from datetime import datetime
 from nose.plugins.skip import Skip, SkipTest
+from logl.post.event_matchup import generate_summary
+import string
+
+#-------------------------
+# helper methods for tests
+#-------------------------
+
+
+def generate_event(target, type, more, w, d):
+    """Generate an event of the specified type.
+    More will be None unless the type specified requires
+    additional fields. w and d may be none."""
+    e = {}
+    e["type"] = type
+    # set type-specific fields
+    if type == "status":
+        e["state"] = more["state"]
+    elif type == "sync":
+        e["sync_to"] = more["sync_to"]
+    elif (string.find(type, "conn") >= 0):
+        e["conn_IP"] = more["conn_IP"]
+        e["conn_number"] = more["conn_number"]
+    e["target"] = target
+    if not w:
+        e["witnesses"] = target
+    else:
+        e["witnesses"] = w
+    e["dissenters"] = d
+    return e
+
+
+def new_frame(servers):
+    """Generate a new frame, with no links, broken_links,
+    syncs, or users, and all servers set to UNDISCOVERED
+    does not set the 'summary' field"""
+    f = {}
+    f["date"] = datetime.now()
+    f["server_count"] = len(servers)
+    f["witnesses"] = []
+    f["dissenters"] = []
+    f["flag"] = False
+    f["links"] = {}
+    f["broken_links"] = {}
+    f["syncs"] = {}
+    f["users"] = {}
+    f["servers"] = {}
+    for s in servers:
+        f["servers"][s] = "UNDISCOVERED"
+        f["links"][s] = []
+        f["broken_links"][s] = []
+        f["users"][s] = []
+        f["syncs"][s] = []
+    return f
 
 
 #---------------------
 # test info_by_type()
 #---------------------
 
+
 def test_info_by_type_status():
     """Test method on status type event"""
-    pass
+    e = generate_event("3", "status", {"state": "PRIMARY"}, "3", None)
+    f = info_by_type(new_frame(["3"]), e)
+    assert f
+    assert f["servers"]["3"] == "PRIMARY"
 
 
 def test_info_by_type_reconfig():
     """Test method on reconfig type event"""
-    pass
+    e = generate_event("1", "reconfig", None, "1", None)
+    f = info_by_type(new_frame(["1"]), e)
+    assert f
 
 
 def test_info_by_type_new_conn():
     """Test method on new_conn type event"""
-    pass
+    e = generate_event("1", "new_conn",
+                       {"conn_IP": "1.2.3.4",
+                        "conn_number": 14}, "1", None)
+    f = info_by_type(new_frame(["1"]), e)
+    assert f
+    assert f["users"]["1"]
+    assert len(f["users"]["1"]) == 1
+    assert "1.2.3.4" in f["users"]["1"]
 
 
 def test_info_by_type_end_conn():
     """Test method on end_conn type event"""
-    pass
+    # first, when there was no user stored
+    e = generate_event("1", "end_conn",
+                       {"conn_IP": "1.2.3.4",
+                        "conn_number": 14}, "1", None)
+    f = info_by_type(new_frame(["1"]), e)
+    assert f
+    assert not f["users"]["1"]
+    # next, when there was a user stored
+    f = new_frame(["1"])
+    f["users"]["1"].append("1.2.3.4")
+    f = info_by_type(f, e)
+    assert f
+    assert not f["users"]["1"]
 
 
 def test_info_by_type_sync():
