bfd3e2ff51b7d3981124143eb36808632ad0d298
Kaushal Parikh <kaushal.parikh@10gen.com>
2012-07-10 14:05:49 -0400

    Made sure that it no longer draws a line from a server to it's self.

--- logl/display/js/render.js
+++ logl/display/js/render.js
@@ -29,48 +29,48 @@ render = function(time) {
 
     for (var name in frames[time]["servers"]) {
         var state = frames[time]["servers"][name];
-	// add logic to parse out ".LOCKED"
-	// to capture actual server state
-	var n = state.split(".");
-	state = n[0];
-	console.log(state);
+    // add logic to parse out ".LOCKED"
+    // to capture actual server state
+    var n = state.split(".");
+    state = n[0];
+    console.log(state);
         switch(state) {
         case "PRIMARY":
-	    primary(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
-	    break;
+        primary(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
+        break;
         case "SECONDARY":
-	    secondary(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
-	    break;
+        secondary(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
+        break;
         case "ARBITER":
-	    arbiter(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
-	    break;
+        arbiter(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
+        break;
         case "DOWN":
-	    down(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
-	    break;
+        down(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
+        break;
         case "RECOVERING":
-	    recovering(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
-	    break;
+        recovering(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
+        break;
         case "ROLLBACK":
-	    rollback(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
-	    break;
+        rollback(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
+        break;
         case "FATAL":
-	    fatal(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
-	    break;
+        fatal(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
+        break;
         case "UNKNOWN":
-	    unknown(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
-	    break;
-	case "UNDISCOVERED":
-	    undiscovered(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
-	    break;
+        unknown(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
+        break;
+        case "UNDISCOVERED":
+        undiscovered(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
+        break;
         case "REMOVED":
-	    removed(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
-	    break;
+        removed(servers[name]["x"], servers[name]["y"], servers[name]["r"], contexts["server"]);
+        break;
         }
 
-	// add lock, if necessary
-	if (n.length == 2) {
-	    lock(servers[name]["x"], servers[name]["y"], servers[name]["r"]);
-	}
+    // add lock, if necessary
+    if (n.length == 2) {
+        lock(servers[name]["x"], servers[name]["y"], servers[name]["r"]);
+    }
         xvals[count] = servers[name]["x"];
         yvals[count] = servers[name]["y"];
         count++;
@@ -78,10 +78,12 @@ render = function(time) {
     var xvals2 = xvals, yvals2 = yvals;
     for(var i = 0; i < count; i++) {
         for(var j = 0; j < count; j++) {
-            one_line(xvals[i], yvals[i], xvals2[j], yvals2[j], contexts["arrow"]);
-            one_arrow(100, 100, 200, 200, contexts["arrow"]);
+            if (xvals[i] != xvals[j])
+                one_arrow(xvals[i], yvals[i], xvals2[j], yvals2[j], contexts["link"]);
         }
     }
+    //one_arrow(100, 100, 200, 200, contexts["arrow"]);
+    //one_line(120, 100, 220, 200, contexts["link"]);
 
 
     }
