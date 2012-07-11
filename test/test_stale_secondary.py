24bacee4029ad66ef998e12f8817e0a314a2aef5
Samantha Ritter <samantha.ritter@10gen.com>
2012-07-11 17:12:24 -0400

    Add tests to capture hostname or IP address

--- test/test_stale_secondary.py
+++ test/test_stale_secondary.py
@@ -12,24 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from logl.filters.stale_secondary import *
 from datetime import datetime
 
 
 def test_criteria():
+    """Test the criteria() method of stale_secondary.py"""
     assert criteria("this should not pass") == -1
     assert criteria("Thu Sep 9 17:22:46 [rs_sync] replSet error RS102 too stale to catch up") == 0
     assert criteria("Thu Sep 9 17:24:46 [rs_sync] replSet error RS102 too stale to catch up, at least from primary: 127.0.0.1:30000") == 0
 
 
 def test_process():
+    """Test the process() method of stale_secondary.py"""
     date = datetime.now()
     check_state("Thu Sep 9 17:22:46 [rs_sync] replSet error RS102 too stale to catch up", 0, date, None)
     check_state("Thu Sep 9 17:24:46 [rs_sync] replSet error RS102 too stale to catch up, at least from primary: 127.0.0.1:30000", 0, date, "127.0.0.1:30000")
+    check_state("Thu Sep 9 17:24:46 [rs_sync] replSet error RS102 too stale to catch up, at least from primary: sam@10gen.com:27017", 0, date, "sam@10gen.com:27017")
     assert process("This should fail", date) == None
 
 
 def check_state(message, code, date, server):
+    """Helper method for tests"""
     doc = process(message, date)
     assert doc
     assert doc["type"] == "stale"
