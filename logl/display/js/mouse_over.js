6580b847500d079e493df5bd571cd3205ff87ea4
Kaushal Parikh <kaushal.parikh@10gen.com>
2012-07-11 16:18:14 -0400

    Added mouse click method

--- logl/display/js/mouse_over.js
+++ logl/display/js/mouse_over.js
@@ -21,39 +21,75 @@ on_canvas_mouseover = function(e) {
     // check if mouse was over server
     for (server in servers) {
 
-	// calculate distance from click
-	diffX = x - servers[server]["x"];
-	diffY = y - servers[server]["y"];
-	distance = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
-	// is it within radius?
-	if (distance <= servers[server]["r"]) {
-	    // check if that server is set to "true" for mouseover
-	    if (!servers[server]["on"]) {
-		// make a shadow under server, and a sound
-		servers[server]["on"] = true;
-		var shadow = contexts["shadow"];
+    // calculate distance from click
+    diffX = x - servers[server]["x"];
+    diffY = y - servers[server]["y"];
+    distance = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
+    // is it within radius?
+    if (distance <= servers[server]["r"]) {
+        // check if that server is set to "true" for mouseover
+        if (!servers[server]["on"]) {
+        // make a shadow under server, and a sound
+        servers[server]["on"] = true;
+        var shadow = contexts["shadow"];
 
-		// draw the drop shadow
-		shadow.beginPath();
-		shadow.arc(servers[server]["x"], servers[server]["y"], servers[server]["r"], 0, 360, false);
-		shadow.strokeStyle = "red";
-		shadow.fillStyle = "rgba(10, 10, 10, 1)";
-		shadow.lineWidth = 10;
-		shadow.shadowColor = "black";
-		shadow.shadowBlur = servers[server]["r"] + 30;
-		shadow.fill();
-		shadow.shadowBlur = 0;
+        // draw the drop shadow
+        shadow.beginPath();
+        shadow.arc(servers[server]["x"], servers[server]["y"], servers[server]["r"], 0, 360, false);
+        shadow.strokeStyle = "red";
+        shadow.fillStyle = "rgba(10, 10, 10, 1)";
+        shadow.lineWidth = 10;
+        shadow.shadowColor = "black";
+        shadow.shadowBlur = servers[server]["r"] + 30;
+        shadow.fill();
+        shadow.shadowBlur = 0;
 
-		// text box over
-		var message = contexts["message"];
-		message.fillStyle = "black";
-		message.rect(x, y, 100, 100);
-	    }
-	}
-	// if not in radius:
-	else if (servers[server]["on"] == true){
-	    servers[server]["on"] = false;
-	    canvases["shadow"].width = canvases["shadow"].width;
-	}
+        // text box over
+        /*var message = contexts["message"];
+        message.fillStyle = "black";
+        message.rect(x, y, 120, 200);
+        message.fill();*/
+        }
     }
+
+    // if not in radius:
+    else if (servers[server]["on"] === true){
+        servers[server]["on"] = false;
+        canvases["shadow"].width = canvases["shadow"].width;
+        canvases["message"].width = canvases["message"].width;
+    }
+    }
+};
+
+on_canvas_click = function(e) {
+    var offset = $("#shadow_layer").offset();
+    var x = e.clientX - offset.left;
+    var y = e.clientY - offset.top;
+    // check if mouse was over server
+    for (server in servers) {
+
+    // calculate distance from click
+    diffX = x - servers[server]["x"];
+    diffY = y - servers[server]["y"];
+    distance = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
+    // is it within radius?
+    if (distance <= servers[server]["r"]) {
+        // check if that server is set to "true" for mouseover
+        if (!servers[server]["on"]) {
+        // make a shadow under server, and a sound
+        var message = contexts["message"];
+        message.fillStyle = "black";
+        message.rect(x, y, 120, 200);
+        message.fill();
+        }
+    }
+
+    // if not in radius:
+    else if (servers[server]["on"] === true){
+        servers[server]["on"] = false;
+        canvases["shadow"].width = canvases["shadow"].width;
+        canvases["message"].width = canvases["message"].width;
+    }
+    }
+
 };
