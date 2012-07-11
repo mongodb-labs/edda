94fecdf8a4a9d1b6a095c492f9e2d7807f379d8d
Kaushal Parikh <kaushal.parikh@10gen.com>
2012-07-11 17:15:20 -0400

    added clicking logic

--- logl/display/js/mouse_over.js
+++ logl/display/js/mouse_over.js
@@ -65,6 +65,10 @@ on_canvas_click = function(e) {
     var offset = $("#shadow_layer").offset();
     var x = e.clientX - offset.left;
     var y = e.clientY - offset.top;
+
+    console.log("I am in the clicking method.");
+    console.log(x);
+    console.log(y);
     // check if mouse was over server
     for (server in servers) {
 
@@ -74,9 +78,54 @@ on_canvas_click = function(e) {
     distance = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
     // is it within radius?
     if (distance <= servers[server]["r"]) {
+        console.log("I am in the clicking method.");
         // check if that server is set to "true" for mouseover
-        if (!servers[server]["on"]) {
         // make a shadow under server, and a sound
+        servers[server]["on"] = true;
+        var shadow = contexts["shadow"];
+
+        // draw the drop shadow
+
+
+        // text box over
+        var message = contexts["message"];
+        message.fillStyle = "black";
+        message.rect(x, y, 120, 200);
+        message.fill();
+        console.log("I am in the clicking method.");
+        }
+    }
+
+    // if not in radius:
+    //else if (servers[server]["on"] === true){
+        //servers[server]["on"] = false;
+        //canvases["shadow"].width = canvases["shadow"].width;
+        //canvases["message"].width = canvases["message"].width;
+    //}
+
+};
+on_canvas_clicks = function(e) {
+    var offset = $("#shadow_layer").offset();
+    var x = e.clientX - offset.left;
+    var y = e.clientY - offset.top;
+    // check if mouse was over server
+    console.log("I am in the clicking method.");
+    console.log(x);
+    console.log(y);
+    for (server in servers) {
+
+    // calculate distance from click
+    diffX = x - servers[server]["x"];
+    diffY = y - servers[server]["y"];
+    distance = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
+    // is it within radius?
+    if (distance <= servers[server]["r"]) {
+        // check if that server is set to "true" for mouseover
+        console.log("I am in this loop.");
+        if (!servers[server]["on"]) {
+            servers[server]["on"] = true;
+        // make a shadow under server, and a sound
+        console.log("I am within the radius of a circle. ");
         var message = contexts["message"];
         message.fillStyle = "black";
         message.rect(x, y, 120, 200);
