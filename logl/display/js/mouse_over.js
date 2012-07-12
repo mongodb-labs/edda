a5fd57147218172bcf6d3dda96d2185831ed65f5
Samantha Ritter <samantha.ritter@10gen.com>
2012-07-12 16:49:11 -0400

    Add message box with information when user clicks on server

--- logl/display/js/mouse_over.js
+++ logl/display/js/mouse_over.js
@@ -43,12 +43,6 @@ on_canvas_mouseover = function(e) {
         shadow.shadowBlur = servers[server]["r"] + 30;
         shadow.fill();
         shadow.shadowBlur = 0;
-
-        // text box over
-        /*var message = contexts["message"];
-        message.fillStyle = "black";
-        message.rect(x, y, 120, 200);
-        message.fill();*/
         }
     }
 
@@ -62,94 +56,50 @@ on_canvas_mouseover = function(e) {
     }
 };
 
-on_canvas_click = function(e) {
+
+new_click = function(e) {
+    // when a user clicks on a server, pop up a box with information about that server
+    var box = document.getElementById("message_box");
     var offset = $("#shadow_layer").offset();
     var x = e.clientX - offset.left;
     var y = e.clientY - offset.top;
 
-    console.log("I am in the clicking method.");
-    console.log(x);
-    console.log(y);
     // check if mouse was over server
-    for (server in servers) {
-
-    // calculate distance from click
-    diffX = x - servers[server]["x"];
-    diffY = y - servers[server]["y"];
-    distance = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
-    // is it within radius?
-    if (distance <= servers[server]["r"]) {
-        console.log("I am in the clicking method.");
-        // check if that server is set to "true" for mouseover
-        // make a shadow under server, and a sound
-        servers[server]["on"] = true;
-        var shadow = contexts["shadow"];
-
-        // draw the drop shadow
-
-
-        // text box over
-        var message = contexts["text"];
-        message.fillStyle = "#1C0E0B";
-        message.rect(x, y, 120, 200);
-        message.fill();
-        console.log("I am in the clicking method.");
-        }
+    var s = is_over_server(e);
+    if (s == null) {
+	box.style.visibility = "hidden";
+	return;
     }
 
-    // if not in radius:
-    //else if (servers[server]["on"] === true){
-        //servers[server]["on"] = false;
-        //canvases["shadow"].width = canvases["shadow"].width;
-        //canvases["message"].width = canvases["message"].width;
-    //}
-
+    var info = "Number: " + s;
+    info += "<br/>Hostname: " + server_names["hostname"][s];
+    info += "<br/>IP: " + server_names["IP"][s];
+    info += "<br/>State: " + frames[current_frame]["servers"][s];
+    box.innerHTML = info;
+    box.style.left = x + "px";
+    box.style.top = y + "px";
+    box.style.visibility = "visible";
 };
-on_canvas_clicks = function(e) {
+
+
+is_over_server = function(e) {
+    // check if a captured event happened over a server
+    // if so, return that server's server_num
     var offset = $("#shadow_layer").offset();
     var x = e.clientX - offset.left;
     var y = e.clientY - offset.top;
+
     // check if mouse was over server
-    console.log("I am in the clicking method.");
-    console.log(x);
-    console.log(y);
     for (server in servers) {
 
-    // calculate distance from click
-    diffX = x - servers[server]["x"];
-    diffY = y - servers[server]["y"];
-    distance = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
-    // is it within radius?
-    if (distance <= servers[server]["r"]) {
-        // check if that server is set to "true" for mouseover
-        console.log("I am in this loop.");
-        if (!servers[server]["on"]) {
-            servers[server]["on"] = true;
-        // make a shadow under server, and a sound
-        console.log("I am within the radius of a circle. ");
-        var message = contexts["message"];
-        message.fillStyle = "black";
-        message.rect(x, y, 120, 200);
-        message.fillStyle = "rgba(0, 0, 200, 0.75)";
-        //setOpacity (message, .5);
-        message.fill();
-        }
+	// calculate distance from click
+	diffX = x - servers[server]["x"];
+	diffY = y - servers[server]["y"];
+	distance = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
+	// is it within radius?
+	if (distance <= servers[server]["r"]) {
+	    return server;
+	}
     }
-
-    // if not in radius:
-    else if (servers[server]["on"] === true){
-        servers[server]["on"] = false;
-        canvases["shadow"].width = canvases["shadow"].width;
-        canvases["message"].width = canvases["message"].width;
-    }
-    }
-
+    return null;
 };
-
-function setOpacity (myElement, opacityValue) {
-    if (window.ActiveXObject) {
-        myElement.style.filter = "alpha(opacity=" + opacityValue*100 + ")"; // IE
-    } else {
-        myElement.style.opacity = opacityValue; // Gecko/Opera
-    }
-}
