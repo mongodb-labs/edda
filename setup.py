712f602c9d21229bce2aa3f57d6dd3bc9052dbef
Kaushal Parikh <kaushalparikh@Kaushals-MacBook-Air.local>
2012-07-17 10:40:46 -0400

    Setup.py now links with pip.

--- setup.py
+++ setup.py
@@ -13,3 +13,38 @@
 # limitations under the License.
 
 # This file will be used with PyPi in order to package and distribute the final product. 
+
+classifiers = """\
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+License :: OSI Approved :: Apache Software License
+Programming Language :: Python
+Programming Language :: JavaScript
+Topic :: Database
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: Unix
+"""
+
+from distutils.core import setup
+import sys
+
+if sys.version_info < (2, 3):
+    _setup = setup
+    def setup(**kwargs):
+        if kwargs.has_key("classifiers"):
+            del kwargs["classifiers"]
+        _setup(**kwargs)
+__doc__ = ""
+doclines = __doc__.split("\n")
+
+setup(name="logl",
+      version=".1",
+      maintainer="10Gen",
+      maintainer_email="zodb-dev@zope.org",
+      url = "https://github.com/kchodorow/logl",
+      license = "http://www.apache.org/licenses/LICENSE-2.0.html",
+      platforms = ["any"],
+      description = doclines[0]
+      classifiers = filter(None, classifiers.split("\n")),
+      long_description = "\n".join(doclines[2:]),
+      )
