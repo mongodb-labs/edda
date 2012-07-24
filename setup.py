18957ceac1130cb1df40cae17ccc0708a671e218
= <kaushal.parikh@10gen.com>
2012-07-24 14:00:28 -0400

    Changed version number and got ready for tagging.
    Signed-off-by: = <kaushal.parikh@10gen.com>

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ __doc__ = ""
 doclines = __doc__.split("\n")
 
 setup(name="edda",
-      version="0.4.0",
+      version="0.5.0",
       maintainer="10Gen",
       maintainer_email="kaushal.parikh@10gen.com",
       #url = "https://github.com/kchodorow/edda",
