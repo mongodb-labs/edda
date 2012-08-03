ea67942e43f8c11487b8cf818cffbd817d8cc108
= <kaushal.parikh@10gen.com>
2012-08-03 13:35:10 -0400

    Version number and syntax changes.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ Programming Language :: Python
 Programming Language :: JavaScript
 Topic :: Database
 Topic :: Software Development :: Libraries :: Python Modules
-Operating System :: Unix :: Windows
+Operating System :: Unix
 """
 
 from distutils.core import setup
@@ -30,34 +30,36 @@ from distutils.core import setup
 __doc__ = ""
 doclines = __doc__.split("\n")
 
-setup(name="edda",
-      version="0.5.3",
-      maintainer="10Gen",
-      maintainer_email="kaushal.parikh@10gen.com",
-      #url = "https://github.com/kchodorow/edda",
-      license="http://www.apache.org/licenses/LICENSE-2.0.html",
-      platforms=["any"],
-      description=doclines[0],
-      classifiers=filter(None, classifiers.split("\n")),
-      long_description="\n".join(doclines[2:]),
-      #include_package_data=True,
-      packages=['edda', 'edda.filters', 'edda.post', 'edda.ui',
-                'edda.sample_logs', 'edda.ui.display.js', 'edda.ui.display.style',
-                'edda.ui.display', 'edda.sample_logs.hp', 'edda.sample_logs.pr'
-               ],
-      #packages = find_packages('src'),  # include all packages under src
-      #package_dir = {'':'src'},   # tell distutils packages are under src
-      scripts=['scripts/edda'],
-      install_requires=['pymongo'],
+setup(
+  name="edda",
+  version="0.6.0",
+  maintainer="10Gen",
+  maintainer_email="kaushal.parikh@10gen.com",
+  #url = "https://github.com/kchodorow/edda",
+  license="http://www.apache.org/licenses/LICENSE-2.0.html",
+  platforms=["any"],
+  description=doclines[0],
+  classifiers=filter(None, classifiers.split("\n")),
+  long_description="\n".join(doclines[2:]),
+  #include_package_data=True,
+  packages=['edda', 'edda.filters', 'edda.post', 'edda.ui',
+            'edda.sample_logs', 'edda.ui.display.js', 'edda.ui.display.style',
+            'edda.ui.display', 'edda.sample_logs.hp', 'edda.sample_logs.pr'
+           ],
+  #packages = find_packages('src'),  # include all packages under src
+  #package_dir = {'':'src'},   # tell distutils packages are under src
+  scripts=['scripts/edda'],
+  install_requires=['pymongo'],
 
-      package_data={
-          # If any package contains *.txt files, include them:
-          'edda.ui.display.js': ['*.js'],
-          'edda.ui.display.style': ['*.css', '*.ico'],
-          'edda.ui.display': ['*.jpg', '*.html'],
-          'edda.sample_logs.hp': ['*.log'],
-          'edda.sample_logs.pr': ['*.log'],
-          # And include any *.dat files found in the 'data' subdirectory
-          # of the 'mypkg' package, also:
-      }
-      )
+  package_data=
+  {
+    # If any package contains *.txt files, include them:
+    'edda.ui.display.js': ['*.js'],
+    'edda.ui.display.style': ['*.css', '*.ico'],
+    'edda.ui.display': ['*.jpg', '*.html'],
+    'edda.sample_logs.hp': ['*.log'],
+    'edda.sample_logs.pr': ['*.log'],
+    # And include any *.dat files found in the 'data' subdirectory
+    # of the 'mypkg' package, also:
+  }
+)
